53b3543a497db623720ab3bfd6e5aa1ecbeb703e
Cash-mar <445dima-bad@mail.ru>
2022-03-16 11:34:10 +0500

    version 1.0.0

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-const DB_HOSTNAME = '89.108.83.110';
-const DB_USERNAME = 'is342_eremin';
-const DB_PASSWORD = 'testingmysql';
-const DB_DATABASE = 'is342_eremin';
+const DB_HOSTNAME = 'mysqlis.radiotech.su';
+const DB_USERNAME = 'is342_14_DB_1';
+const DB_PASSWORD = '36709orstd';
+const DB_DATABASE = 'is342_14_DB_1';
 const DB_PORT = '3306'; # Default 3306
